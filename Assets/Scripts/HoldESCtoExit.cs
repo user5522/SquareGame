f88dc5a47285c99b470220b374898790e493d103
User5522 <oueslatim268@gmail.com>
2022-07-10 17:00:28 +0100

    implement HoldESCToExit
    fading animation comming soon

--- Assets/Scripts/HoldESCtoExit.cs
+++ Assets/Scripts/HoldESCtoExit.cs
@@ -2,21 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HoldESCtoExit : MonoBehaviour {
+public class HoldESCToExit : MonoBehaviour {
 
-    public float startTime = 0f;
-    public float randomTime = 0f;
-    public float holdTime = 5.0f;
-     
-    void Update()
+   // Timer controls
+    private float startTime = 0f;
+    private float timer = 0f;
+    public float holdTime = 4.5f;
+ 
+    private bool held = false;
+ 
+    public KeyCode Key = KeyCode.Escape;
+   
+    void Update ()
     {
-        if (Input.GetKey(KeyCode.Escape))
+        if (Input.GetKeyDown(Key))
         {
-            randomTime = Time.time;
-            if (startTime + holdTime <= Time.time) {
-                Debug.Log("exit"); // this is for testing purposes
-                Application.Quit();
-                }
+            startTime = Time.time;
+            timer = startTime;
+        }
+ 
+        // Adds time onto the timer so long as the key is pressed
+        if (Input.GetKey(Key) && held == false)
+        {
+            timer += Time.deltaTime;
+ 
+            // Once the timer float has added on the required holdTime, changes the bool (for a single trigger), and calls the function
+            if (timer >= (startTime + holdTime))
+            {
+                held = true;
+                Function();
+            }
+        }
+ 
+        // For single effects. Remove if not needed
+        if (Input.GetKeyUp(Key))
+        {
+            held = false;
         }
     }
-} 
+ 
+    // Method called after held for required time
+    void Function()
+    {
+        Debug.Log("works");
+        Application.Quit();
+    }
+}
