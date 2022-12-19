0c7936ffba27fa8600aa54f30b29ef96856ea032
Username * <oueslatim268@gmail.com>
2022-12-19 12:21:32 +0100

    new player killing system

--- Assets/Scripts/KillPlayer.cs
+++ Assets/Scripts/KillPlayer.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class KillPlayer : MonoBehaviour{
     
-    public int respawn;
-    
-    void OnTriggerEnter2D(Collider2D other){
-        if(other.CompareTag("Player")){
-            SceneManager.LoadScene(respawn);
+public Transform respawnPoint;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("DeathTrigger"))
+        {
+            // Teleport the player to the respawn point
+            transform.position = respawnPoint.position;
         }
     }
 }
