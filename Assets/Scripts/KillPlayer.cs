498fa2609b371dda2be4f664a7e7d2d58e9b2f96
Username * <oueslatim268@gmail.com>
2022-12-19 13:13:37 +0100

    add shake effect when player dies/respawns

--- Assets/Scripts/KillPlayer.cs
+++ Assets/Scripts/KillPlayer.cs
@@ -6,10 +6,13 @@ public class KillPlayer : MonoBehaviour{
     
 public Transform respawnPoint;
 
+    public CameraShake cameraShake;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("DeathTrigger"))
         {
+            cameraShake.shakeDuration = 0.75f;
             // Teleport the player to the respawn point
             transform.position = respawnPoint.position;
         }
