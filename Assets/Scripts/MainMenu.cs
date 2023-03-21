db41413707c017f5c8f54376f1dbad9ed34a6bd1
Username * <oueslatim268@gmail.com>
2023-03-21 17:12:49 +0100

    Add a tutorial levels button in the main menu and fix all the animations

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour{
-
-    public void ExitButton(){
+public class MainMenu : MonoBehaviour
+{
+    public void ExitButton()
+    {
         Application.Quit();
         Debug.Log("Quit!"); // This is just for testing purposes
     }
 
-    public void StartButton(){
+    public void TutorialButton()
+    {
         SceneManager.LoadScene(1);
-    } 
+    }
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene(3);
+    }
 }
