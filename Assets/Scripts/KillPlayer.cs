6c63577097bae7e149d96d4abfbb24aeb2172032
Username * <oueslatim268@gmail.com>
2023-03-21 14:42:37 +0100

    improve the player killer script

--- Assets/Scripts/KillPlayer.cs
+++ Assets/Scripts/KillPlayer.cs
@@ -2,15 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KillPlayer : MonoBehaviour{
-    
-public Transform respawnPoint;
-
+public class KillPlayer : MonoBehaviour
+{
+    public Transform respawnPoint;
+    public float deathRange;
     public CameraShake cameraShake;
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("DeathTrigger"))
+        {
+            cameraShake.shakeDuration = 0.75f;
+            // Teleport the player to the respawn point
+            transform.position = respawnPoint.position;
+        }
+        if (Mathf.Abs(transform.position.y - gameObject.transform.position.y) < deathRange)
         {
             cameraShake.shakeDuration = 0.75f;
             // Teleport the player to the respawn point
