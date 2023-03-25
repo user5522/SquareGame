c8a613c45e591e8cb91b299edc88b5dd990a0684
Username * <oueslatim268@gmail.com>
2023-03-26 00:53:18 +0100

    feat: implement a pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    private bool isPaused = false;
+    public GameObject pauseMenu;
+    public Animator pauseIconAnimator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused == false)
+            {
+                Time.timeScale = 0;
+                isPaused = true;
+                pauseMenu.SetActive(isPaused);
+                pauseIconAnimator.SetTrigger("ScaleDown");
+            }
+            else if (isPaused == true)
+            {
+                pauseIconAnimator.SetTrigger("ScaleUp");
+                StartCoroutine(WaitForAnimationToEnd("ScaleUp"));
+                isPaused = false;
+                Time.timeScale = 1;
+            }
+        }
+    }
+
+    IEnumerator WaitForAnimationToEnd(string animationName)
+    {
+        yield return new WaitForSeconds(0.2f); // Wait for 20ms
+
+        while (pauseIconAnimator.GetCurrentAnimatorStateInfo(0).IsName(animationName))
+        {
+            yield return null;
+        }
+
+        pauseMenu.SetActive(isPaused);
+    }
+}
