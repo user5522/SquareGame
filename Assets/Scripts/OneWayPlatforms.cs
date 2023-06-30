937e01496b116596f603be6fb0b460cd355f80bb
Username * <oueslatim@pm.me>
2023-06-30 13:52:03 +0100

    fix one way platforms bugs

--- Assets/Scripts/OneWayPlatforms.cs
+++ Assets/Scripts/OneWayPlatforms.cs
@@ -5,21 +5,35 @@ using UnityEngine;
 public class OneWayPlatforms : MonoBehaviour
 {
     private PlatformEffector2D effector;
+    private bool fallThrough;
 
     void Start()
     {
         effector = GetComponent<PlatformEffector2D>();
     }
 
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        fallThrough = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        fallThrough = false;
+        effector.rotationalOffset = 0f;
+    }
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKeyDown(KeyCode.DownArrow) && fallThrough)
         {
             effector.rotationalOffset = 180f;
+            fallThrough = false;
         }
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow) && (fallThrough = false))
         {
             effector.rotationalOffset = 0;
+            fallThrough = true;
         }
     }
 }
