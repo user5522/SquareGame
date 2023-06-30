1171575030dfc3707c9a24c186f073aad3888ba9
Username * <oueslatim@pm.me>
2023-06-30 13:53:21 +0100

    Update Finish.cs

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -5,11 +5,11 @@ public class Finish : MonoBehaviour
 {
     public int targetScene;
 
-    // Update is called once per frame
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "finish") {
-        SceneManager.LoadScene(targetScene);
+        if (collision.gameObject.tag == "finish")
+        {
+            SceneManager.LoadScene(targetScene);
         }
     }
 }
